e30304b2eb6ce42107829bb8ea1d4f8e764d539f
TrainingCode <greenwind.01011994@gmail.com>
2021-03-27 23:34:54 +0700

    tiếp

--- a.js
+++ a.js
@@ -3,8 +3,24 @@
 let soLanMua = 0;
 let soMuaDuoc = -1;
 
-while (soMuaDuoc !=93) {
+/*while (soMuaDuoc !=93) {
     soMuaDuoc = Math.floor(Math.random()*100);
     const message = 'lan thu ' + ++soLanMua + ', mua duoc so ' + soMuaDuoc;
     console.log(message);
+}*/
+// có quá nhiều dấu cộng, nếu mình không muốn thì mình có thể làm như sau :
+// $ là sintag
+while (soMuaDuoc !=93) {
+    soMuaDuoc = Math.floor(Math.random()*100);
+    const message = `lan thu  ${ ++soLanMua } mua duoc so ${ soMuaDuoc}`;
+    console.log(message);
 }
+console.log(eval('4 + 5'));//không nên lạm dụng dễ bị hack nếu truyền thêm toán tử random, ngoại trừ cộng trừ nhân chia
+
+/*for (; soMuaDuoc !=93; ) {
+    soMuaDuoc = Math.floor(Math.random()*100);
+    const message = 'lan thu ' + ++soLanMua + ', mua duoc so ' + soMuaDuoc;
+    console.log(message);
+    
+}*/
+// mình muốn in ra dấu ' ' ;
--- ngay2/d.js
+++ ngay2/d.js
@@ -12,3 +12,36 @@ console.log(a);
 if(a>4 || --a>3) {
     console.log(a);
 }
+
+switch(a) {
+    case 1:
+        console.log('MOT');
+    case 2:
+        console.log('HAI');
+    default:
+        console.log('khong tim thay');
+        break;
+}
+
+//while chặc chẽ hơn do while bởi vì vòng lặp đầu tiên đã kiểm tra điều kiện rồi.
+//nếu muốn vòng lặp đầu tiên chưa kiểm tra điều kiện  ngay thì ta dùng do while
+
+for(let i =1; i <= 4; i++) {
+    console.log(i);
+}
+
+for(let i =1; true; i++){
+    if(i > 5 ) break;
+    console.log(i);
+}
+
+console.log('while');
+
+let i = 1;
+while ( i<= 2) console.log(i++);
+
+console.log('do while');
+
+do {
+    console.log(i++);
+} while (i <= 100);
