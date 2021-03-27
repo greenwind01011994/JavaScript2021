c3fa8660d7659137ec9c7fc9d3943bbfe911cdd9
TrainingCode <greenwind.01011994@gmail.com>
2021-03-27 22:42:08 +0700

    tiep

---
+++ ngay2/c.js
@@ -0,0 +1,8 @@
+//console.log(-1/0);
+//null (khong co), undefined(khong ton tai), NaN(sinh ra khi cộng một số không tồn tại), Infinity ( chia 1 số cho 0)
+
+let a;
+console.log(a);
+console.log(a + 5 * 5);
+console.log(null+5);
+console.log(-1/0);
