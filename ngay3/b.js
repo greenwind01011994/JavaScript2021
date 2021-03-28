e897f118541dd97e7b48cd8dced8ef8026ac7325
TrainingCode <greenwind.01011994@gmail.com>
2021-03-28 11:23:10 +0700

    xon

---
+++ ngay3/b.js
@@ -0,0 +1,15 @@
+/*
+        *
+       ***
+      *****
+     *******
+      *****
+       ***
+        *
+    soDong = 7 (soDong luon % 2 = 1) 
+*/
+function veHinh3(soDong) {
+    for( let i = 1; i <= soDong; i++) {
+        
+    }
+}
