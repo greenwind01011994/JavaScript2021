6f21c4bdb67adb78036e7d2a122c650793391ef4
TrainingCode <greenwind.01011994@gmail.com>
2021-03-30 14:34:00 +0700

    hoàn thành

--- ngay7-EXPRESS/index.js
+++ ngay7-EXPRESS/index.js
@@ -28,9 +28,86 @@ app.get('/chao/:name', (req, res) => {  ///4. ta truyên vào ('/ 1 route = link
     const age = req.params.age;
     res.send('xin chào toi la ${name}, ${age} tuoi '); /// 1 rout in nhiều link khác 
 });
+    // viết gọn hơn
+    app.get('/hello/:name/:age', (req, res) => {
+    const {name,age} = req.params;
+    res.send('xin chào toi la ${name}, ${age} tuoi '); /// 1 rout in nhiều link khác 
+});
+
+    //ở đây mình còn rout nữa 
+    app.get('/tinh/:tenPhepTinh/:soA/:soB', (req, res) => {
+    //tenPhepTinh CONG TRU NHAN CHIA
+    //so tu nhien
+    //instance
+    //locallhost: 3000/tinh/CONG/3/4 -> '3 + 4 = 7'
+    
+    //khi có function dài, code dài thì không khuyến khích viết vào trong đây, ta tạo ra class
+});
+    class Tinh {
+        constructor(tenPhepTinh, soA, soB) {
+            //trong này thì gán
+            this.tenPhepTinh = tenPhepTinh;
+            this.soA = soA;
+            this.soB = soB;
+        }
+        get ResulString() {
+            // ở đây return '3 + 4 = 7'
+            let chuoiPhepTinh;
+            if(this.tenPhepTinh ==='CONG') chuoiPhepTinh = `${this.soA} + {this.soB}`
+            
+            //cách viết ngắn hơn đẹp hơn
+            let chuoiPhepTinh;
+            const {tenPhepTinh, soA, soB} = this
+            chuoiPhepTinh = `${soA} /${soB}`
+            if tenPhepTinh ==='CONG') chuoiPhepTinh = `${soA} + ${soB}`;
+            if tenPhepTinh ==='TRU') chuoiPhepTinh = `${soA} - ${soB}`;
+            if tenPhepTinh ==='NHAN') chuoiPhepTinh = `${soA} * ${soB}`;
+            const result = eval(chuoiPhepTinh);
+            return `${chuoiPhepTinh} = ${result};
+
+            //return `${chuoiPhepTinh} = ${eval(chuoiPhepTinh)}`
+            //mặt định phép tính là chia vì nếu không phải phếp tinh cộng trừ nhân chia thì sẽ không bị lỗi            
+        }
+    }
+    const pt = new Tinh('CONG', 10, 11);
+    console.log(pt.getResultString());
+
+    //chúng ta có thể tách code cho đẹp
+    getChuoiPhepTinh() {
+        let chuoiPhepTinh;
+        const {tenPhepTinh, soA, soB} = this
+        chuoiPhepTinh = `${soA} /${soB}`
+        if tenPhepTinh ==='CONG') chuoiPhepTinh = `${soA} + ${soB}`;
+        if tenPhepTinh ==='TRU') chuoiPhepTinh = `${soA} - ${soB}`;
+        if tenPhepTinh ==='NHAN') chuoiPhepTinh = `${soA} * ${soB}`;
+        return chuoiPhepTinh;
+    }
+    getResultString() {
+        const chuoiPhepTinh = this.getChuoiPhepTinh     //không nên gọi getChuoiPhepTinh vì nó sẽ hiểu là function ở ngoài 
+        const result = eval(chuoiPhepTinh);
+        return `${chuoiPhepTinh} = ${result};
+    }
+
+    //làm gọn và đẹp hơn nũa. đây là cách tách hàm 
+    getChuoiPhepTinh() {
+        const {tenPhepTinh, soA, soB} = this
+        if tenPhepTinh ==='CONG') return `${soA} + ${soB}`;
+        if tenPhepTinh ==='TRU') return  `${soA} - ${soB}`;
+        if tenPhepTinh ==='NHAN') return `${soA} * ${soB}`;
+        return `${soA} / ${soB}`;
+    }
+    getResultString() {
+        const chuoiPhepTinh = this.getChuoiPhepTinh     //không nên gọi getChuoiPhepTinh vì nó sẽ hiểu là function ở ngoài 
+        const result = eval(chuoiPhepTinh);
+        return `${chuoiPhepTinh} = ${result};
+    }
+    //chạy
+    const a = new Tinh('CONG', 3, 4);
+    console.log(a.getResultString());
 */ 
 
-app.get('/tinh/:tenPhepTinh/:soA/:soB', (req, res) => {
+app.get('/tinh/:tenPhepTinh/:soA/:soB', (req, res) => {     //5. tách nó ra class dễ xử lý và đẹp, được cái là chúng ta tập hợp các phương thức mà hay sử dụng trong lớp đối tượng
+                                                            //lại 1 chỗ để kiểm tra 1 lượt  và tái sử dụng nhiều chỗ, khi muốn sử dụng phương thức thì tạo new object và gọi thôi
     const { tenPhepTinh, soA, soB } = req.params;
     const pt = new PhepTinh(tenPhepTinh, soA, soB);
     res.send(pt.getResultString());
