01cb7ca23824651c4b6bcbf80d94eb6e7ec6d656
TrainingCode <greenwind.01011994@gmail.com>
2021-04-05 15:19:04 +0700

    thay doi

--- ngay8-EJS/index.js
+++ ngay8-EJS/index.js
@@ -2,9 +2,26 @@
 ///phần học từ đầu
 const express = require('express');
 const reload = require('reload');   //eslint-disable-line
+
+//5. ta có mảng arryPeople    ****chú ý ta không thể viết mảng ở đây được sẽ sinh lỗi, phải viết sau : const app = express(); vì không có biến app để gọi
+/*const arrPeople = [
+    { name: 'Ti', age: 10 },
+    { name: 'Teo', age: 11},
+    { name: 'Tun', age: 12},
+];*/
+//ta có cách truyền thứ nhất để bên learn.ejs hiểu arrPeople ở đâu
+//app.locals.arrPeople = arrPeople; //bên này app.locals.Têngi thì bên kia có tên đó
+
 // template engine
 const app = express();
 
+const arrX = [
+    { name: 'Ti', age: 10 },
+    { name: 'Teo', age: 11},
+    { name: 'Tun', age: 12},
+];
+app.locals.arrPeople = arrX;// không phải cứ tên biên là arryPeople thì gi là arrypeople
+//Cách truyền thứ 2 
 //1. thư mục các file views nằm ở views-ht
 app.set('views', './views-ht'); //đang xét views là tên thuộc tính của nó chưa phải tên thu mục nên phải ghi đúng //
 //đây là câu lệnh viết thừa khi đặt tên views-ht là views (mặc định), nên viết vào cho đủ nếu 2 cái ghi khác nhau
@@ -16,9 +33,18 @@ app.set('view engine', 'ejs');  ///
 app.get('/', (req, res) => res.sendfile('./views-ht/a.html'));// truy cập vào file html
 */
 app.get('/', (req, res) => res.render('home')); // render ra file    //home là home.ejs ; render là gửi code thành giao diện
+
 //3. tạo ra 1 rout mới
-app.get('/learn', (req, res) => res.render('learn'));
+//app.get('/learn', (req, res) => res.render('learn'));
+
+//5. cách truyền thứ 2, câu lênh render có 2 tham số, tham số thứ 2 là 1 oject có thuộc tính username thì bên ejs sẽ thành tên biến username 
+//app.get('/learn', (req, res) => res.render('learn', { username: 'phong'}));
+app.get('/learn', (req, res) =>{
+    res.render('learn', { username: 'phong1'});
+});
+//còn cách 1 : app.locals. phù hợp các thông tin mà người dùng đều dùng chung
 
 //4.truyền app để reload 
 reload(app);
+
 app.listen(3000, () => console.log('Server started'));
