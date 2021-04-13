b66ce8d7c113a1200d9961e8841990bd95438daf
TrainingCode <greenwind.01011994@gmail.com>
2021-04-13 15:27:51 +0700

    tạo

--- ngay10-multer/index.js
+++ ngay10-multer/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const reload = require('reload');
 
 const app = express();
 
+app.set('views', './views');
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('home'));
 
+reload(app);
 app.listen(3000, () =>console.log('Server Started'));
