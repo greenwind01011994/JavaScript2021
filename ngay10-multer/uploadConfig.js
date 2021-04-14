77398b4c530a62f5097c67e662f31924d4c7dbeb
TrainingCode <greenwind.01011994@gmail.com>
2021-04-14 14:57:11 +0700

    a

--- ngay10-multer/uploadConfig.js
+++ ngay10-multer/uploadConfig.js
@@ -3,15 +3,15 @@ const storage = multer.diskStorage({
     destination: (req,file, cb) => cb(null, './public'),
     filename: (req, file, cb) => cb(null, file.originalname),
 });
-function fileFilter(req, file, cb) {
+/*function fileFilter(req, file, cb) {
     const {mimetype} = file;
     if(mimetype === 'image/jpg' || mimetype === 'image/png') { 
         return cb(null, true);
     } 
     cb(new Error('File khong dung dinh dang! '));
 }
+*/
 
-
-const upload = multer({storage, fileFilter});
+const upload = multer({storage});
 
 module.exports = upload; // chuyển code file qua
