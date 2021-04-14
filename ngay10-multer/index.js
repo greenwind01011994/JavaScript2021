0ca0bace6881449d130bee487e896fc758d5cb9c
TrainingCode <greenwind.01011994@gmail.com>
2021-04-14 09:31:36 +0700

    parser

--- ngay10-multer/index.js
+++ ngay10-multer/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const parser = require('body-parser').urlencoded({extended: false});
+
 const reload = require('reload');
 
 const app = express();
@@ -6,6 +8,10 @@ const app = express();
 app.set('views', './views');
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('home'));
+
+app.post('/signup', parser, (req,res) => {
+    res.send(req.body);
+});
 
 reload(app);
 app.listen(8080, () =>console.log('Server Started'));
