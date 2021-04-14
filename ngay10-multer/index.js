21686d129f7d9d2583ab88b97de532f921e25653
TrainingCode <greenwind.01011994@gmail.com>
2021-04-14 10:31:10 +0700

    luu

--- ngay10-multer/index.js
+++ ngay10-multer/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const parser = require('body-parser').urlencoded({extended: false});
+//const parser = require('body-parser').urlencoded({extended: false});
+const multer = require('multer');
+const upload = multer({dest: './public'});
 
 const reload = require('reload');
 
@@ -9,7 +11,12 @@ app.set('views', './views');
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('home'));
 
-app.post('/signup', parser, (req,res) => {
+/* khi ta khai báo : const parser = require('body-parser').urlencoded({extended: false}); thì ta dùng cái này
+app.post('/signup', parser, (req,res) => { //thay vì parser thì ta thay upload 1 file
+    res.send(req.body);
+});
+*/
+app.post('/signup', upload.single('profileSingle'), (req,res) => { //upload.single('tên  của input file') => trả cho ta 1 function 3 tham số ; đừng hỏi tại sao truyền tên như vậy vì ta sử dụng thư viện, mà thư viện yêu cầu thì mình làm theo
     res.send(req.body);
 });
 
