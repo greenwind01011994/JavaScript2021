0e47493798e7202e1cf56d0b360a3dc56fadc320
TrainingCode <greenwind.01011994@gmail.com>
2021-04-19 15:01:48 +0700

    luu

--- ngay11/a.js
+++ ngay11/a.js
@@ -1,2 +1,9 @@
 //bất đồng bộ -> sử dụng hoặc các tài nguyên nằm ngoài chương trình
 //đồng bộ -> sử dụng các giá trị nằm trong chương trình
+
+//để đọc file x.txt nằm ngoài chương trình 
+//để đọc file này ra require đến 1 thư viện hay 1 modul có sẵn 
+const fs = require('fs');//fs là thư viện có sẵn trong nodeJS
+
+//đọc file thì
+fs.readFile('./x.txt')
