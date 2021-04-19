78ef2041c2fbdc91d4f44d29a6c8180bc2c76b78
TrainingCode <greenwind.01011994@gmail.com>
2021-04-19 17:03:04 +0700

    luu

--- ngay11/c.js
+++ ngay11/c.js
@@ -1,7 +1,8 @@
 const request = require('request');
 
-    function cong(a, b, cb) {
-        request(`http://localhost:8080/tinh/CONG/${a}/${b}`, (err, res, body) => {
+/*
+function cong(a, b, cb) {
+    request(`http://localhost:8080/tinh/CONG/${a}/${b}`, (err, res, body) => {
         cb(err, body);
     });
 }
@@ -9,4 +10,13 @@ cong(4,5, (err, kq) => {
     if(err) return console.log(err.message);
     console.log('ket qua: ' + kq);
 });
-
+*/
+function cong(a, b, cb) {
+    request(`http://localhost:8080/tinh/CONG/${a}/${b}`, (err, res, body) => {
+        cb(err, body);
+    });
+}
+cong(4,5, (err, kq) => {
+    if(err) return console.log(err.message);
+    console.log('ket qua: ' + kq);
+});
