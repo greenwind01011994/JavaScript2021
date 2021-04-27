a696e36bf467d77f587ba1679f9efdd4aecf3830
TrainingCode <greenwind.01011994@gmail.com>
2021-04-27 08:40:21 +0700

    luu

--- ngay13-Mongo/b.js
+++ ngay13-Mongo/b.js
@@ -4,8 +4,8 @@ const url = 'mongodb://localhost:27017/test';
 
 MongoClient.connect(url)
 .then(db => {
-    const test = db.collection('users');
-    return test.find().toArray();
+    const users = db.collection('users');
+    return users.find().toArray();
 })
 .then(result => console.log(result))
-.catch(err => console.log(err.message))
+.catch(err => console.log(err.message));
